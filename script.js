36c4ee35302d8b3c76272d654035f07178e610fc
Gabe Young <gabriel_young@emerson.edu>
2020-02-04 16:29:00 -0500

    completed HTML and JS

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-
-var d = new Date();
-
-
-//on load functions
+ //on load functions
 $(document).ready(function(){
     //display date
-    $("#date").html("<h1>" + d + "<h1>")
+    $("#date").html(moment().format("MMM Do YY"));
     //post contents of local storage
-    $("#textarea" + num ).html(localStorage.savedData);
+    //$("#textarea" + num ).html(localStorage.savedData);
+    $("#hour-9 .tasks").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .tasks").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .tasks").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .tasks").val(localStorage.getItem("hour-12"));
+    $("#hour-1 .tasks").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .tasks").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .tasks").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .tasks").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .tasks").val(localStorage.getItem("hour-5"));
+
+$(".save").on("click", function(){
+    console.log("button pressed")
+    var time = $(this).parent().attr("id");
+    console.log(time);
+    var task = $(this).siblings(".tasks").val();
+    console.log(task);
+    localStorage.setItem(time, task);
+})
 });
 
+
 //save text to local storage
-function save(element){
-    var num = element.attr("id");
+//function save(element){
+  //  var num = element.attr("id");
     //specify which field will be saved and pulls text content
-    var IDdata= $("#textarea" + num).val();
-    console.log(IDdata);
+    //var IDdata= $("#textarea" + num).val();
+    //console.log(IDdata);
     //create an array for user content to be stored
-   var userList = $("<li>" + IDdata);
-    userList.append(document.createTextNode(IDdata));
-    var myArray = (userList, IDdata);
+   //var userList = $("<li>" + IDdata);
+    //userList.append(document.createTextNode(IDdata));
+    //var myArray = (userList, IDdata);
     //post the content to the local storage memory
-    localStorage.setItem("savedData", JSON.stringify(myArray));
-    console.log(localStorage.getItem('savedData'));
-};
+    //localStorage.setItem("savedData", JSON.stringify(myArray));
+    //console.log(localStorage.getItem('savedData'));
+//};
+
